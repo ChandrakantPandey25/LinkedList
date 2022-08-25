4b5ff5011cb7ef4d17388751f2f16b39bb4b8054
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-24 20:47:32 -0700

    Added Link56_30_70

---
+++ LinkedList/LinkedList/Link56_30_70.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class Link56_30_70
+    {
+        public Node Head;
+        public Node Tail;
+
+        public Link56_30_70()
+        {
+            Head = null;
+            Tail = null;
+        }
+        public void AddNode(Node node)
+        {
+            if(Head==null && Tail==null)
+            {
+                Head = node;
+                Tail = node;
+
+            }
+            else
+            {
+                node.next = Head;
+                Head = node;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("LinkedList is empty");
+            else
+                Console.WriteLine("Linked List:");
+            while(temp!=null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                    Console.Write("->");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Link56_30_70 link = new Link56_30_70();
+            Node node1 = new Node(56);
+            Node node2 = new Node(30);
+            Node node3 = new Node(70);
+            link.AddNode(node1);
+            link.AddNode(node2);
+            link.AddNode(node3);
+            link.Display();
+        }
+    }
+}
