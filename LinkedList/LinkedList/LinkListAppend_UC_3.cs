ef7b0bcd5873b153878fb57f718c8e4ade4ee469
Chandrakant <chandrakant.pandey2014@yahoo.com>
2022-08-26 16:22:03 -0700

    Added LinklistAppend

---
+++ LinkedList/LinkedList/LinkListAppend_UC_3.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class LinkListAppend_UC_3
+    {
+        public Node Head;
+        public Node Tail;
+        public LinkListAppend_UC_3()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public void Append(int data)
+        {
+           
+            Node node = new Node(data);
+            
+            if (this.Head == null)
+            {
+                this.Head = node;
+            }
+
+            else
+            {
+                
+                Node temp = Head;
+
+                while (temp.next != null)
+                
+                {
+                    
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+
+            Console.WriteLine("Appended the Node:" + data);
+        }
+        internal void Display()
+        {
+            Node temp = Head;
+            if (temp == null)
+                Console.WriteLine("LinkedList is empty");
+            else
+                Console.WriteLine("Linked List:");
+            while (temp != null)
+            {
+                Console.Write(temp.data);
+                if (temp.next != null)
+                    Console.Write("->");
+                temp = temp.next;
+            }
+        }
+    }
+}
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -9,6 +9,7 @@ namespace LinkedList
             Console.WriteLine("Welcome to LinkedList Program");
             Console.WriteLine("Enter 1 to create simple linkedlist");
             Console.WriteLine("Enter 2 to create Link_56_30_70 linkedlist");
+            Console.WriteLine("Enter 3 to create LinkListAppend_UC_3 linkedlist");
 
 
             switch(Convert.ToInt32(Console.ReadLine()))
@@ -24,16 +25,27 @@ namespace LinkedList
                 link.AddNode(node2);
                 link.Display();
                 break;
-                case 2:
+            case 2:
                     Link56_70_30 linkOne = new Link56_70_30();
                     Node node4 = new Node(70);
-                    Node node5 = new Node(30);
-                    Node node6 = new Node(56);
                     linkOne.AddNode(node4);
+                    Node node5 = new Node(30);
                     linkOne.AddNode(node5);
+                    Node node6 = new Node(56);                  
                     linkOne.AddNode(node6);
                     linkOne.Display();
                     break;
+                case 3:
+                    LinkListAppend_UC_3 linkTwo = new LinkListAppend_UC_3();
+                    Node node7 = new Node(30);
+                    Node node8 = new Node(56);
+                    Node node9 = new Node(70);
+                    linkTwo.Append(56);
+                    linkTwo.Append(30);
+                    linkTwo.Append(70);                    
+                    linkTwo.Display();
+                    break;
+
             }
         }
     }
